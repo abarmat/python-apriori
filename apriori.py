7138286a566e60a4884a1e9de23e6a09fa50daf4
Ariel Barmat <abarmat@gmail.com>
2016-04-18 03:16:08 -0300

    Initial code commit

---
+++ apriori.py
@@ -0,0 +1,124 @@
+import argparse
+from itertools import chain, combinations
+
+
+def joinset(itemset, k):
+    return set([i.union(j) for i in itemset for j in itemset if len(i.union(j)) == k])
+
+
+def subsets(itemset):
+    return chain(*[combinations(itemset, i + 1) for i, a in enumerate(itemset)])
+    
+
+def itemset_from_data(data):
+    itemset = set()
+    transaction_list = list()
+    for row in data:
+        transaction_list.append(frozenset(row))
+        for item in row:
+            if item:
+                itemset.add(frozenset([item]))
+    return itemset, transaction_list
+
+
+def itemset_with_min_support(transaction_list, itemset, min_support):
+    len_transaction_list = len(transaction_list)
+    l = [
+        (item, float(sum(1 for row in transaction_list if item.issubset(row)))/len_transaction_list) 
+        for item in itemset
+    ]
+    return dict([(item, support) for item, support in l if support >= min_support])
+
+
+def freq_itemset(transaction_list, c_itemset, min_support):
+    f_itemset = dict()
+
+    k = 1
+    while True:
+        if k > 1:
+            c_itemset = joinset(l_itemset, k)
+        l_itemset = itemset_with_min_support(transaction_list, c_itemset, min_support)
+        if not l_itemset:
+            break
+        f_itemset = dict(f_itemset, **l_itemset)
+        k += 1
+
+    return f_itemset    
+
+
+def apriori(data, min_support, min_confidence):
+    # Get first itemset and transactions
+    itemset, transaction_list = itemset_from_data(data)
+
+    # Get the frequent itemset
+    f_itemset = freq_itemset(transaction_list, itemset, min_support)
+
+    # Association rules
+    rules = list()
+    for item, support in f_itemset.items():
+        if len(item) > 1:
+            for A in subsets(item):
+                B = item.difference(A)
+                if B:
+                    A = frozenset(A)
+                    AB = A | B
+                    support = itemset_with_min_support(transaction_list, [AB, A], 0)
+                    confidence = float(support[AB]) / support[A]
+                    if confidence >= min_confidence:
+                        rules.append((A, B, confidence))    
+    return rules, f_itemset
+
+
+def print_report(rules, f_itemset):
+    print('--Frequent Itemset--')
+    for item, support in sorted(f_itemset.items(), key=lambda (item, support): support):
+        print('[I] {} : {}'.format(tuple(item), round(support, 4)))
+
+    print('')
+    print('--Rules--')
+    for A, B, confidence in sorted(rules, key=lambda (A, B, confidence): confidence):
+        print('[R] {} => {} : {}'.format(tuple(A), tuple(B), round(confidence, 4))) 
+
+
+def data_from_csv(filename):
+    f = open(filename, 'rU')
+    for l in f:
+        row = map(str.strip, l.split(','))
+        yield row
+
+
+def parse_options():
+    optparser = argparse.ArgumentParser(description='Apriori Algorithm.')
+    optparser.add_argument(
+        '-f', '--input_file',
+        dest='filename',
+        help='filename containing csv',
+        required=True
+    )
+    optparser.add_argument(
+        '-s', '--min_support',
+        dest='min_support',
+        help='minimum support',
+        default=0.25,
+        type=float
+    )
+    optparser.add_argument(
+        '-c', '--min_confidence',
+        dest='min_confidence',
+        help='minimum confidence',
+        default=0.5,
+        type=float
+    )
+    return optparser.parse_args()
+
+
+def main():
+    options = parse_options()
+
+    data = data_from_csv(options.filename)
+    rules, itemset = apriori(data, options.min_support, options.min_confidence)
+    print_report(rules, itemset)
+
+
+if __name__ == '__main__':
+    main()
